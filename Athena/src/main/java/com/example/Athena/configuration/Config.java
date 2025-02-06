ff7f7909cf8424853f637ce4c41c9589f16e6946
access-denied-001 <devang2000singh@gmail.com>
2025-02-07 03:04:08 +0530

    Update backend cors

---
+++ Athena/src/main/java/com/example/Athena/configuration/Config.java
@@ -0,0 +1,14 @@
+package com.example.Athena.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
