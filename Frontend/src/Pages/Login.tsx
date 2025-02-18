386fb9f293c2d11bcfdf00fff45d6141e7a3ca3a
access-denied-001 <devang2000singh@gmail.com>
2025-02-19 00:07:48 +0530

    Minor fix

--- Frontend/src/Pages/Login.tsx
+++ Frontend/src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "../redux/store";
 import { useNavigate } from "react-router-dom";
@@ -7,6 +7,7 @@ import { buttonVariants } from "../components/Button";
 import { IUser, setCredentials } from "../features/mainSlice";
 
 const Login = () => {
+    const [phoneNumber, setPhoneNumber] = useState("");
     const { accessToken } = useSelector((state: IRootState) => state.main);
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -21,7 +22,7 @@ const Login = () => {
         //will add login feature
         const dummyUser: IUser = {
             name: "Devang",
-            phoneNumber: "8114430657",
+            phoneNumber: phoneNumber,
             role: "ADMIN",
         };
 
@@ -36,7 +37,25 @@ const Login = () => {
     return (
         <div className="h-full flex flex-col justify-center items-center px-6">
             <div className="bg-gray-300 py-12 px-6 rounded-xl flex flex-col items-center">
-                <Input />
+                <div className="p-4 rounded-lg max-w-96">
+                    <label className="text-white text-md font-bold">
+                        Phone Number
+                    </label>
+                    <div className="relative mt-2">
+                        <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r-2 pr-2">
+                            <select className="text-sm rounded-lg h-full bg-transparent">
+                                <option>IN +91</option>
+                            </select>
+                        </div>
+                        <input
+                            type="tel"
+                            placeholder="Phone number here"
+                            className="w-full pl-[6rem] pr-3 py-2 appearance-none bg-white outline-none border focus:border-slate-600 shadow-sm rounded-lg"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </div>
+                </div>
                 <button
                     className={classNames(
                         buttonVariants({
@@ -50,26 +69,6 @@ const Login = () => {
                 >
                     Login
                 </button>
-            </div>
-        </div>
-    );
-};
-
-const Input = () => {
-    return (
-        <div className="p-4 rounded-lg max-w-96">
-            <label className="text-white text-md font-bold">Phone Number</label>
-            <div className="relative mt-2">
-                <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r-2 pr-2">
-                    <select className="text-sm rounded-lg h-full bg-transparent">
-                        <option>IN +91</option>
-                    </select>
-                </div>
-                <input
-                    type="tel"
-                    placeholder="Phone number here"
-                    className="w-full pl-[6rem] pr-3 py-2 appearance-none bg-white outline-none border focus:border-slate-600 shadow-sm rounded-lg"
-                />
             </div>
         </div>
     );
